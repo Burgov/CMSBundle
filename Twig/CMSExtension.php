5849d4aabd0ff2efbb6083e1747211a17a507242
Bart van den Burg <bart@burgov.nl>
2013-07-29 15:11:56 +0200

    option to disable read-more link

--- Twig/CMSExtension.php
+++ Twig/CMSExtension.php
@@ -28,11 +28,11 @@ class CMSExtension extends \Twig_Extension
         );
     }
 
-    public function readmore($html, $linkTarget)
+    public function readmore($html, $linkTarget, $linkIfNoReadMore = true)
     {
         $parts = preg_split('#<p.*?><span.*?>(\s|&nbsp;)*\.{3}(\s|&nbsp;)*</span></p>#', $html);
 
-        return $parts[0].'<p class="readmore"><a href="'.$linkTarget.'">Lees meer...</a></p>';
+        return $parts[0].($linkIfNoReadMore || count($parts) > 1 ? '<p class="readmore"><a href="'.$linkTarget.'">Lees meer...</a></p>' : '');
     }
 
     public function article($html)
